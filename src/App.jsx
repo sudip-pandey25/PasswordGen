af2999fc0b3b445645becc215f63930a11e82413
Sudip Shakya <mainsudip25@gmail.com>
2025-01-19 22:34:57 +0545

    UI change

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,42 @@ function App() {
 
   return (
     <>
-      <div className="mx-auto text-3xl text-center my-[20%] ">
-        <h1>Password Generator</h1>
+      <div className="w-full max-w-md shadow-md rounded-lg mx-auto text-center my-[20%] px-4  bg-gray-700 py-4 ">
+        <h1 className="text-green-400">Password Generator</h1>
+        <div className="flex shadow-sm rounded-lg overflow-hidden mb-4 mt-2">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            onChange={(e) => setPassword(e.target.value)}
+            readOnly
+          />
+          <button className="outline-none bg-green-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={8}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label className="text-white">Length:{length}</label>
+          </div>
+          <div className="flex text-sm gap-x-0.5">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => setNumberAllowed((prev) => !prev)}
+            />
+            <label className="text-white">Numbers</label>
+          </div>
+        </div>
       </div>
     </>
   );
