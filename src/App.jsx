207138cf08baa0e9281e05c703a29db9b9813f4f
Sudip Shakya <mainsudip25@gmail.com>
2025-01-19 21:57:54 +0545

    Second Commit Logical Part

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./index.css";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGeneration = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "123456789";
+    if (charAllowed) str += "!@#$%^&*()_+";
+
+    for (let i = 1; i <= Array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold font-mono mx-auto ">Hello world</h1>
+      <div className="mx-auto text-3xl text-center my-[20%] ">
+        <h1>Password Generator</h1>
+      </div>
     </>
   );
 }
